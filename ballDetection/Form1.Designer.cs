b2d3340651a31ac19708cd2456a6ee1f0047a6d7
jukkridb59 <jukkridb59@email.nu.ac.th>
2019-07-23 03:14:51 +0700

    Ball detection first commit

---
+++ ballDetection/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ballDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tlpOuter = new System.Windows.Forms.TableLayoutPanel();
+            this.tlpInner = new System.Windows.Forms.TableLayoutPanel();
+            this.ibOriginal = new Emgu.CV.UI.ImageBox();
+            this.ibThresh = new Emgu.CV.UI.ImageBox();
+            this.btnPauseOrResume = new System.Windows.Forms.Button();
+            this.txtXYRadius = new System.Windows.Forms.TextBox();
+            this.tlpOuter.SuspendLayout();
+            this.tlpInner.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibThresh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tlpOuter
+            // 
+            this.tlpOuter.ColumnCount = 2;
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpOuter.Controls.Add(this.tlpInner, 0, 1);
+            this.tlpOuter.Controls.Add(this.ibOriginal, 0, 0);
+            this.tlpOuter.Controls.Add(this.ibThresh, 1, 0);
+            this.tlpOuter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpOuter.Location = new System.Drawing.Point(0, 0);
+            this.tlpOuter.Name = "tlpOuter";
+            this.tlpOuter.RowCount = 2;
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tlpOuter.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tlpOuter.Size = new System.Drawing.Size(800, 450);
+            this.tlpOuter.TabIndex = 0;
+            // 
+            // tlpInner
+            // 
+            this.tlpInner.ColumnCount = 2;
+            this.tlpOuter.SetColumnSpan(this.tlpInner, 2);
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tlpInner.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.Controls.Add(this.btnPauseOrResume, 0, 0);
+            this.tlpInner.Controls.Add(this.txtXYRadius, 1, 0);
+            this.tlpInner.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpInner.Location = new System.Drawing.Point(3, 363);
+            this.tlpInner.Name = "tlpInner";
+            this.tlpInner.RowCount = 1;
+            this.tlpInner.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpInner.Size = new System.Drawing.Size(794, 84);
+            this.tlpInner.TabIndex = 0;
+            // 
+            // ibOriginal
+            // 
+            this.ibOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibOriginal.Enabled = false;
+            this.ibOriginal.Location = new System.Drawing.Point(3, 3);
+            this.ibOriginal.Name = "ibOriginal";
+            this.ibOriginal.Size = new System.Drawing.Size(394, 354);
+            this.ibOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibOriginal.TabIndex = 2;
+            this.ibOriginal.TabStop = false;
+            // 
+            // ibThresh
+            // 
+            this.ibThresh.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ibThresh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ibThresh.Enabled = false;
+            this.ibThresh.Location = new System.Drawing.Point(403, 3);
+            this.ibThresh.Name = "ibThresh";
+            this.ibThresh.Size = new System.Drawing.Size(394, 354);
+            this.ibThresh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ibThresh.TabIndex = 2;
+            this.ibThresh.TabStop = false;
+            // 
+            // btnPauseOrResume
+            // 
+            this.btnPauseOrResume.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPauseOrResume.AutoSize = true;
+            this.btnPauseOrResume.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnPauseOrResume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPauseOrResume.Location = new System.Drawing.Point(3, 27);
+            this.btnPauseOrResume.Name = "btnPauseOrResume";
+            this.btnPauseOrResume.Size = new System.Drawing.Size(64, 30);
+            this.btnPauseOrResume.TabIndex = 0;
+            this.btnPauseOrResume.Text = "Pause";
+            this.btnPauseOrResume.UseVisualStyleBackColor = true;
+            this.btnPauseOrResume.Click += new System.EventHandler(this.BtnPauseOrResume_Click);
+            // 
+            // txtXYRadius
+            // 
+            this.txtXYRadius.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtXYRadius.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtXYRadius.Location = new System.Drawing.Point(73, 3);
+            this.txtXYRadius.Multiline = true;
+            this.txtXYRadius.Name = "txtXYRadius";
+            this.txtXYRadius.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtXYRadius.Size = new System.Drawing.Size(718, 78);
+            this.txtXYRadius.TabIndex = 1;
+            this.txtXYRadius.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tlpOuter);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tlpOuter.ResumeLayout(false);
+            this.tlpInner.ResumeLayout(false);
+            this.tlpInner.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ibOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ibThresh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpOuter;
+        private System.Windows.Forms.TableLayoutPanel tlpInner;
+        private System.Windows.Forms.Button btnPauseOrResume;
+        private System.Windows.Forms.TextBox txtXYRadius;
+        private Emgu.CV.UI.ImageBox ibOriginal;
+        private Emgu.CV.UI.ImageBox ibThresh;
+    }
+}
+
